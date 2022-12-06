76d8842ae3acd21ab13c63e83e72607afde2dd32
Arthur <arthurfelixcruz@gmail.com>
2022-12-05 22:47:44 -0300

    achando o meio da frase

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var frase = "o argollo e o melhor";
 function ContarVogais(frase) {
     var indexVogal = 0;
     var letras = frase.split("");
-    var vogais = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-    letras.forEach(function (letra) {
-        vogais.forEach(function (vogal) {
-            if (vogal = letra) {
+    var vogais = ["a", "e", "i", "o", "u"];
+    vogais.forEach(function (vogal) {
+        letras.forEach(function (letra) {
+            if (vogal == letra) {
                 indexVogal++;
             }
         });
@@ -13,3 +13,19 @@ function ContarVogais(frase) {
     return indexVogal;
 }
 console.log(ContarVogais(frase));
+var palavra = "maluco";
+function MeioDaFrase(palavra) {
+    var meio = palavra.split("");
+    var index = meio.length;
+    var metade;
+    if (meio.length % 2 == 0) {
+        index = index / 2;
+        metade = meio[index - 1] + meio[index];
+    }
+    else {
+        index = Math.round(index / 2);
+        metade = meio[index];
+    }
+    return metade;
+}
+console.log(MeioDaFrase(palavra));
--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ var frase:string = "o argollo e o melhor"
 function ContarVogais(frase:string):number{
     let indexVogal:number = 0
     let letras:string[] = frase.split("")
-    let vogais = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    let vogais = ["a", "e", "i", "o", "u"]
     
-    letras.forEach(letra => {
-        vogais.forEach(vogal => {
-           if (vogal = letra) {
+    vogais.forEach(vogal => {
+        letras.forEach(letra => {
+           if (vogal == letra) {
             indexVogal++
            } 
         });
@@ -16,3 +16,36 @@ function ContarVogais(frase:string):number{
 }
 
 console.log(ContarVogais(frase))
+
+var palavra:string = " pneumoultramicroscopicossilicovulcanoconiotico"
+
+function MeioDaFrase(palavra:string):string {
+    let meio:string[] = palavra.split("")
+    let index:number = meio.length
+    let metade:string
+
+    if (meio.length % 2 == 0) {
+        index = index / 2
+        metade = meio[index - 1] + meio[index]
+    }else {
+        index = Math.round(index / 2)
+        metade = meio[index]
+    }
+
+    return metade
+}
+
+console.log(MeioDaFrase(palavra))
+
+var numero:number = 5793
+
+function AcharMaiorNumero(numero:number):number{
+    let numeroString = numero.toString()
+    let array = numeroString.split("")
+    array = array.sort((a,b) => parseInt(b) - parseInt(a))
+    let array2 = array.toString().replace(/,/g, "")
+    
+    return parseInt(array2)
+}
+
+console.log(AcharMaiorNumero(numero));
